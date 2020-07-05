cec296d19fc61ae137fe8241d69d681e22b7fd43
Jordana <jordana.cbac@gmail.com>
2020-07-04 21:03:12 -0300

    Changing comments, removing useless code and changing the slide test

--- value_and_policy_iteration.py
+++ value_and_policy_iteration.py
@@ -111,113 +111,127 @@ def create_df_states(states):
 
 	return(df_t)
 
-def iterative_policy_evaluation(df, states_view, proper_policy, goal, initial, lastState):#df iteracao de Vpi
+def iterative_policy_evaluation(df, states_view, proper_policy, goal, initial, last_state):
+	# Making a copy of first proper_policy
 	past_proper_policy = proper_policy.copy()
-	current_proper_policy ={} #store current proper policy
-	t = time.time()
-	for i in df.columns:
-		df.loc[0, i] = random.randint(0,10)
 	
-	df_residual = df.copy()
-	for i in df_residual.columns:
-		df_residual.loc[0, i] = 999
-	df_residual.loc[0, goal] = 0
+	# Structure to store current proper policy
+	current_proper_policy = {}
 
-	#epsilon = 0.1
+	# t variable to count the algorithm execution time
+	t = time.time()
+
+	# Generating the pseudo random numbers for each state (numbers interval from 0 to 10) - V0 values:
+	for col in df.columns:
+		df.loc[0, col] = random.randint(0,10)
+
+	# First iteration:
 	n = 1
 	for state in df.columns:
+		# Receiving pi action for each state based on the proper policy first received 
 		PIn = proper_policy[state]
+		# If the state is not a goal, then it is necessary to:
 		if(state != goal):
+			# Calculate the approximation evaluation of PIn
 			Vpin = policy_evaluation(state, PIn, df, states_view, n)
-			df_residual.loc[n-1, state] = abs(Vpin - df.loc[n-1, state]) 
+			# Update value
 			df.loc[n, state] = Vpin
+		# The cost of the goal to reach itself will always be zero
 		else:
-			df_residual.loc[n-1, state] = 0
 			df.loc[n, state] = 0
-	# TODO: while max residual < epsilon
+
+	# Other necessary iterations
 	while(True):
 		n += 1
 		
 		for state in df.columns:
 			if(state == goal):
-				df_residual.loc[n-1, state] = 0
+				# The cost of the goal to reach itself will always be zero
 				df.loc[n, state] = 0
 				current_proper_policy[state] = "-"
 				continue
-			# calculate bellman backup
-			#agr ao inves de receber o melhor valor recebe a melhor acao, acao q tem menor custo
+
+			# Calculates Bellman backup to return the best action (smallest cost)
 			PIn = bellman_backup(state, states_view, df, n, 1)
+			# Updates current_proper_policy with action returned from Bellman backup
 			current_proper_policy[state] = PIn
+			# Policy evaluation step
 			Vpin = policy_evaluation(state, PIn, df, states_view, n)
-
-			#compute residual
-			df_residual.loc[n-1, state] = abs(Vpin - df.loc[n-1, state]) 
-
-			# update Value
+			# Updates value
 			df.loc[n, state] = Vpin
-
-		# check epsilon
-		df_check = df_residual.loc[n-1]
-		#print(df_check)
-		max_residual = float(df_check.max())
-		#print(max_residual)
-		#if max_residual < epsilon:
-			#break
-		#if past and current policy arent equals, reset current proper policy and store past policy with current
-		if(past_proper_policy==current_proper_policy): 
+		
+		# Stop condition: pi(n+1) == pi(n)
+		if (past_proper_policy == current_proper_policy): 
 			break
+		
+		# Updates past_proper_policy with the current policy
 		past_proper_policy = current_proper_policy.copy()
+		
+		# Clears the current policy
 		current_proper_policy.clear()
+	
 	t = time.time() - t
 	print("Policy iteration: " + str(n) + " iterations, runtime: " + str(t) )
-	buildGrid(current_proper_policy, lastState, initial)
+	
+	# Calling method responsible for the grid visualization
+	buildGrid(current_proper_policy, last_state, initial)
 
 	return
 
+# Method responsible for the policy evaluation. It computes V(n+1), the approximation evaluation of PIn
 def policy_evaluation(state, action, df_values, states_view, n):
-	for i in states_view[state]:
-		if(action in i[0]):#acao escolhida,calcular Vn
-			if len(i) == 4:
-				return ( float(i[1]) + float(df_values.loc[n-1, i[2]] ) )
+	
+	for act in states_view[state]:
+		# If the action is found in the list of possible actions for the state, then it is necessary to calculate Vn
+		if(action in act[0]):
+			# Checks if there is more than 1 resulting state for the action
+			if (len(act) == 4):
+				return (float(act[1]) + float(df_values.loc[n-1, act[2]]))
+			# There is more than 1 resulting state for the action, so it is necessary to compute the probability
 			else:
-				# if it has more than 1 result, compute the probability
 				Q_value = 0
-				for num_states in range (2, len(i), 2):
-					Q_value += ( float(i[num_states+1]) * float(df.loc[n-1, i[num_states]]) )
-				return (float(i[1]) + Q_value ) 
+				for num_states in range (2, len(act), 2):
+					Q_value += ( float(act[num_states+1]) * float(df.loc[n-1, act[num_states]]) )
+				return (float(act[1]) + Q_value) 
 
-# Bellman backup algorithm calculates Qn(s, a) for each iteration (n) and returns the minimum value
+# Bellman backup algorithm calculates Qn(s, a) for each iteration (n) and returns the minimum value or best action 
 def bellman_backup(state, states_view, df, n, ret):
 	# ret = 0: returns value
 	# ret = 1: returns action
 
-	# Next steps: Calculate Q*(state, action) and V*(state)
-	#df vai ser a tabela com tds valores de cada iteracao n
-	#Calc Q*
-	# get actions
+	# Getting actions
 	actions = states_view.get(state)
-	Q = [] #para devolver menor valor
-	best_action = ["none",1000000] #para devolver melhor acao
+	
+	# Q is used to return smallest value (in other words, best value)
+	Q = []
+
+	# best_action is self explanatory 
+	best_action = ["none", 1000000]
+
 	if actions is not None:
 		for action in actions:
-			# check if it has more than 1 state for each move
-			if len(action) == 4:
-				resultado = float(action[1]) + float(df.loc[n-1, action[2]])
-				Q.append(resultado)
+			# Checks if there is more than 1 resulting state for the action
+			if (len(action) == 4):
+				result = float(action[1]) + float(df.loc[n-1, action[2]])
+				# Stores result in Q
+				Q.append(result)
+			# There is more than 1 resulting state for the action, so it is necessary to compute the probability
 			else:
-				# if it has more than 1 result, compute the probability
 				Q_value = 0
 				for num_states in range (2, len(action), 2):
 					Q_value += ( float(action[num_states+1]) * float(df.loc[n-1, action[num_states]]) )
-				resultado = float(action[1]) + Q_value 
-				Q.append(resultado)
-			if(resultado<best_action[1]): 
+				result = float(action[1]) + Q_value
+				# Stores result in Q
+				Q.append(result)
+
+			if (result < best_action[1]): 
 					best_action[0] = action[0]
-					best_action[1] = resultado 
+					best_action[1] = result
+
 		if(ret == 0):
-			return( float(min(Q)) )	
+			return (float(min(Q)))	
 		else:
-			return best_action[0]
+			return (best_action[0])
 
 def value_iteration(df, states_view, goal, initial, last_state):
 	# Pseudo random numbers generated by the following seed:
@@ -273,28 +287,29 @@ def value_iteration(df, states_view, goal, initial, last_state):
 
 		# Stop condition: max residual < epsilon
 		if max_residual < epsilon:
-			df.to_csv("replace.csv")
-			for i in df_result.columns:
-				df_result.loc[0, i] = df.loc[n, i]
-			print(df_result)
 			break
 
 	t = time.time() - t
 	print("Value iteration: " + str(n) + " iterations, runtime: " + str(t) )
 
+	# Storing the best action for each state
 	best_action={}
 	for state in df_result.columns:
 		best_action[state] = bellman_backup(state, states_view, df, n, 1)
-	best_action[goal] = "-"#set goal state equals to '-', so this wont be null 
-		#df_result.loc[1, state] = bellman_backup(state, states_view, df, n, 1)
+	# Setting goal state equals to '-', so this won't be null 
+	best_action[goal] = "-"
+	
+	# Calling method responsible for the grid visualization
 	buildGrid(best_action, last_state, initial)
 
 	return
 
+# Method responsible for the grid visualization
 def buildGrid(statesANDactions, lastState, initial):
-	index = lastState.split("x")[1].split("y") #[0]=row [1]=column
+
+	index = lastState.split("x")[1].split("y")
 	grid = [ [ "*" for y in range(int(index[0])+1) ] for x in range(int(index[1])+1) ] 
-	#grid = np.zeros((int(index[0]),int(index[1])))
+
 	sort_states = sorted(statesANDactions.keys())
 	action = ""
 	for i in statesANDactions:
@@ -303,10 +318,9 @@ def buildGrid(statesANDactions, lastState, initial):
 		elif("east" in statesANDactions[i]): action = "→"
 		elif("west" in statesANDactions[i]): action = "←"
 		else: action = "G"
-		index = str(i).split("x")[1].split("y") #[0]=row [1]=column
+		index = str(i).split("x")[1].split("y")
 		grid[int(index[0])][int(index[1])] = action
-		#pensar em como fazer a matriz
-	#initial state
+
 	index = initial.split("x")[1].split("y")
 	if(grid[int(index[0])][int(index[1])] == "↑"): grid[int(index[0])][int(index[1])] ="▲"
 	elif(grid[int(index[0])][int(index[1])] == "↓"): grid[int(index[0])][int(index[1])] ="▼"
@@ -315,8 +329,8 @@ def buildGrid(statesANDactions, lastState, initial):
 
 	for x in reversed(grid):
 		print(x)
-	#tratar o estado inicial e os q nao tem nada
 
+	return
 
 
 if __name__ == '__main__':
@@ -325,15 +339,12 @@ if __name__ == '__main__':
 	parser = ArgumentParser()
 	parser.add_argument("-f", dest="filename", help="domain file to be read")
 	parser.add_argument("-pp", dest="proper_policy", help="proper policy to start policy iteration")
-	#parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
 	args = parser.parse_args()
 	
 	# Generating a list for the file content and reading it
 	content_list = []
 	with open(args.filename) as file:
-		content = file.readlines()
-	'''with open("TestesGrid/FixedGoalInitialState/navigation_1.net") as file:
-		content = file.readlines()'''
+		content = file.readlines()	
 	
 	# Replacing "\t", spliting the file on line breaks and filling in the list with the file's content
 	for element in content:
@@ -358,19 +369,16 @@ if __name__ == '__main__':
 	# Calling the method responsible to create a dataframe from the list of states
 	df = create_df_states(states)
 	# To execute the slides example:
-	#df.loc[0] = {'robot-at-x0y0': float(3), 'robot-at-x0y1': float(3), 'robot-at-x1y0-1': float(2), 'robot-at-x2y0': float(0), 'robot-at-x2y1': float(1), 'robot-at-x2y2': float(2)}
+	#df.loc[0] = {'robot-at-x0y0': float(3), 'robot-at-x0y1': float(3), 'robot-at-x1y0': float(2), 'robot-at-x2y0': float(0), 'robot-at-x2y1': float(1), 'robot-at-x2y2': float(2)}
 
 	# Calling value iteration method:
-	value_iteration(df, states_view,goal,initial,states[-1])
-
+	value_iteration(df, states_view, goal, initial, states[-1])
 
 	with open(args.proper_policy) as json_file:
 		proper = json.load(json_file)	
-	#reset the variable
+	
+	# Resetting the variable
 	df = create_df_states(states)
+	
 	# Calling policy iteration method:
 	iterative_policy_evaluation(df,states_view,proper,goal,initial,states[-1])
-
-	'''with open("PoliticasFixedRandom/FixedGoalInitialState/navigation_1.net_politicas.json") as json_file:
-		proper = json.load(json_file)
-	iterative_policy_evaluation(df,states_view,proper,goal,initial,states[-1])'''
